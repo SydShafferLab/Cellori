35213b063f60f51037f6be04b3e21c1232e00fa0
William Niu <wniu721@gmail.com>
2022-04-26 21:58:39 -0400

    Add custom EfficientNetV2XS architecture.

--- cellori/model_zoo/efficientnetv2_config.py
+++ cellori/model_zoo/efficientnetv2_config.py
@@ -1,4 +1,43 @@
 DEFAULT_BLOCKS_ARGS = {
+    "efficientnetv2-xs": [
+        {
+            "kernel_size": (3, 3),
+            "num_repeat": 4,
+            "input_filters": 24,
+            "output_filters": 24,
+            "expand_ratio": 1,
+            "se_ratio": 0.0,
+            "strides": 1,
+            "conv_type": 1,
+        }, {
+            "kernel_size": (3, 3),
+            "num_repeat": 8,
+            "input_filters": 24,
+            "output_filters": 48,
+            "expand_ratio": 4,
+            "se_ratio": 0.0,
+            "strides": 2,
+            "conv_type": 1,
+        }, {
+            "conv_type": 0,
+            "expand_ratio": 4,
+            "input_filters": 48,
+            "kernel_size": (3, 3),
+            "num_repeat": 8,
+            "output_filters": 64,
+            "se_ratio": 0.25,
+            "strides": 2,
+        }, {
+            "conv_type": 0,
+            "expand_ratio": 6,
+            "input_filters": 64,
+            "kernel_size": (3, 3),
+            "num_repeat": 16,
+            "output_filters": 128,
+            "se_ratio": 0.25,
+            "strides": 2,
+        }
+    ],
     "efficientnetv2-s": [
         {
             "kernel_size": (3, 3),
