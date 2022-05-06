7b65db898cd397f912fa74de9c1bda77bb69bbe5
William Niu <wniu721@gmail.com>
2022-05-05 21:31:52 -0400

    Generalize data loading function to allow only train or test dataset.

--- cellori/applications/cyto/data.py
+++ cellori/applications/cyto/data.py
@@ -8,45 +8,35 @@ from skimage import measure
 from cellori.utils.dynamics import masks_to_flows
 from cellori.utils.transforms import normalize, RandomAffine
 
-def load_dataset(train, test, use_gpu=True):
 
-    def load_folder(folder):
+def load_dataset(folder, calculate_gradients=True, use_gpu=True):
 
-        images = []
-        masks = []
+    images = []
+    masks = []
 
-        image_files = sorted(glob.glob(os.path.join(folder, '*img.png')))
-        mask_files = sorted(glob.glob(os.path.join(folder, '*masks.png')))
+    image_files = sorted(glob.glob(os.path.join(folder, '*img.png')))
+    mask_files = sorted(glob.glob(os.path.join(folder, '*masks.png')))
 
-        for image_file, mask_file in zip(image_files, mask_files):
+    for image_file, mask_file in zip(image_files, mask_files):
 
-            image = imread(image_file)[:, :, :2]
-            mask = imread(mask_file)
+        image = imread(image_file)[:, :, :2]
+        mask = imread(mask_file)
 
-            images.append(image)
-            masks.append(mask)
+        images.append(image)
+        masks.append(mask)
 
-        return images, masks
+    if calculate_gradients:
+        gradients = [np.moveaxis(masks_to_flows(mask, use_gpu=use_gpu), 0, -1) for mask in masks]
+    else:
+        gradients = None
 
-    images_train, masks_train = load_folder(train)
-    images_test, masks_test = load_folder(test)
-
-    gradients_train = [np.moveaxis(masks_to_flows(mask, use_gpu=use_gpu), 0, -1) for mask in masks_train]
-    gradients_test = [np.moveaxis(masks_to_flows(mask, use_gpu=use_gpu), 0, -1) for mask in masks_test]
-
-    train_ds = {
-        'image': images_train,
-        'mask': masks_train,
-        'gradients': gradients_train
+    ds = {
+        'image': images,
+        'mask': masks,
+        'gradients': gradients
     }
 
-    test_ds = {
-        'image': images_test,
-        'mask': masks_test,
-        'gradients': gradients_test
-    }
-
-    return train_ds, test_ds
+    return ds
 
 
 def transform_dataset(ds, key, resize_diameter=30, output_shape=(256, 256)):
@@ -92,6 +82,7 @@ def transform_dataset(ds, key, resize_diameter=30, output_shape=(256, 256)):
 
     transformed_ds = {
         'image': np.array(images),
+        'mask': np.array(masks)[:, :, :, None],
         'gradients': np.array(gradients),
         'semantic': np.array([mask > 0 for mask in masks])[:, :, :, None]
     }
