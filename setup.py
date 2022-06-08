9259d991cf37b98322587e4020a382719e31ed96
William Niu <wniu721@gmail.com>
2022-06-08 17:47:55 -0400

    Prepare for release of version 4.0.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = "4.0"
+DESCRIPTION = "Cellori"
+LONG_DESCRIPTION = "A fast and robust algorithm for whole-cell segmentation."
+
+setup(
+    name="cellori",
+    version=VERSION,
+    author="William Niu",
+    author_email="<wniu721@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['fastremap', 'flax', 'imageio', 'numba', 'numpy', 'opencv-python', 'scikit-image', 'scipy',
+                      'tifffile', 'torch', 'tqdm'],
+    include_package_data=True,
+    keywords=["segmentation"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
