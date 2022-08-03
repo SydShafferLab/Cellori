1fadffc71a68cee6213f79fbeb046cf42e7f0792
William Niu <wniu721@gmail.com>
2022-08-03 16:21:25 -0400

    Add DeepTile-enabled dataset generation function to Spots.

--- cellori/applications/spots/data.py
+++ cellori/applications/spots/data.py
@@ -1,12 +1,77 @@
+import deeptile
 import numpy as np
 
+from jax import random
 from pathlib import Path
 from scipy import ndimage
 
 from cellori.utils.transforms import batch_normalize, batch_standardize, RandomAugment, subpixel_distance_transform
 
 
-def load_datasets(path, adjustment='normalize'):
+def generate_dataset(path, key, adjustment='normalize',
+                     tile_size=(256, 256), overlap=(0.1, 0.1), min_spots=2,
+                     train_size=0.8, valid_size=0.1):
+
+    image_list = []
+    coords_list = []
+
+    path = Path(path)
+    if path.is_file() and path.suffix == '.npz':
+        datasets = [path]
+    else:
+        datasets = path.glob('*.npz')
+
+    for npz in datasets:
+
+        data = np.load(npz, allow_pickle=True)
+
+        image_list.append(data['x'])
+        coords_list.append(data['y'])
+
+    image_list = np.concatenate(image_list)
+    coords_list = np.concatenate(coords_list, dtype=object)
+
+    if adjustment == 'normalize':
+        image_list = np.asarray(batch_normalize(image_list))
+    elif adjustment == 'standardize':
+        image_list = np.asarray(batch_standardize(image_list))
+
+    dt = deeptile.load(image_list)
+    tiles1 = dt.get_tiles(tile_size, overlap)
+    tiles2 = tiles1.import_data(coords_list, 'coords')
+    nonempty_tiles = [(image, coords) for image, coords in
+                      zip(np.concatenate(tiles1.nonempty_tiles), np.concatenate(tiles2.nonempty_tiles))
+                      if len(coords) > min_spots]
+    tiled_images, tiled_coords = zip(*nonempty_tiles)
+    tiled_images = np.array(tiled_images)
+    tiled_coords = np.array(tiled_coords, dtype=object)
+
+    size = len(tiled_images)
+    perms = np.asarray(random.permutation(key, size))
+    tiled_images = tiled_images[perms]
+    tiled_coords = tiled_coords[perms]
+
+    split_indices = np.rint(np.cumsum((train_size, valid_size)) * size).astype(int)
+    x_train = tiled_images[:split_indices[0]]
+    y_train = tiled_coords[:split_indices[0]]
+    x_valid = tiled_images[split_indices[0]:split_indices[1]]
+    y_valid = tiled_coords[split_indices[0]:split_indices[1]]
+    x_test = tiled_images[split_indices[1]:]
+    y_test = tiled_coords[split_indices[1]:]
+
+    dataset = {
+        'x_train': x_train,
+        'y_train': y_train,
+        'x_valid': x_valid,
+        'y_valid': y_valid,
+        'x_test': x_test,
+        'y_test': y_test
+    }
+
+    return dataset
+
+
+def load_datasets(path):
 
     train = {'images': [], 'coords': []}
     valid = {'images': [], 'coords': []}
@@ -35,15 +100,6 @@ def load_datasets(path, adjustment='normalize'):
     valid['coords'] = np.concatenate(valid['coords'])
     test['images'] = np.concatenate(test['images'])
     test['coords'] = np.concatenate(test['coords'])
-
-    if adjustment == 'normalize':
-        train['images'] = np.asarray(batch_normalize(train['images']))
-        valid['images'] = np.asarray(batch_normalize(valid['images']))
-        test['images'] = np.asarray(batch_normalize(test['images']))
-    elif adjustment == 'standardize':
-        train['images'] = np.asarray(batch_standardize(train['images']))
-        valid['images'] = np.asarray(batch_standardize(valid['images']))
-        test['images'] = np.asarray(batch_standardize(test['images']))
 
     ds = {
         'train': train,
