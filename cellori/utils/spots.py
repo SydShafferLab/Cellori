8522849709385b8bac3816692e457a9f063486f1
William Niu <wniu721@gmail.com>
2022-08-17 17:41:21 -0400

    More efficient subpixel coordinate calculation.

--- cellori/utils/spots.py
+++ cellori/utils/spots.py
@@ -2,46 +2,25 @@ import jax.numpy as np
 import numpy as onp
 
 from jax import jit, vmap
-from jax.lax import scan
-from skimage import feature, filters
+from skimage import feature
 
 
-def compute_spot_coordinates(deltas, labels, scale=1, min_distance=1, threshold=1.5):
+def compute_spot_coordinates(deltas, labels, min_distance=1, threshold=1.5):
 
-    counts, convergence = jit(colocalize_pixels)(deltas, labels)
-    adjusted_counts = filters.gaussian(onp.asarray(counts + labels[:, :, 0] - 1), sigma=0.25)
-    adjusted_counts = adjusted_counts * (scale ** 2)
+    counts = jit(colocalize_pixels)(deltas * labels)
+    adjusted_counts = onp.asarray(counts + labels[:, :, 0] - 1)
     peaks = feature.peak_local_max(adjusted_counts,
                                    min_distance=min_distance, threshold_abs=threshold, exclude_border=False)
     num_peaks = len(peaks)
     if num_peaks > 0:
-        _, coords = scan(compute_subpixel_coords, (counts, convergence, peaks), np.arange(num_peaks))
-        coords = onp.asarray(coords)
+        coords = peaks + onp.asarray(deltas)[peaks[:, 0], peaks[:, 1]]
     else:
         coords = onp.empty((0, 2), dtype=onp.float32)
 
     return coords, adjusted_counts
 
 
-def compute_subpixel_coords(carry, i):
-
-    counts, convergence, peaks = carry
-    peak = peaks[i]
-    sources = _search_convergence(convergence, peak[0], peak[1])
-    coords = np.sum(convergence * sources[:, :, None], axis=(0, 1)) / counts[peak[0], peak[1]]
-
-    return carry, coords
-
-
-def colocalize_pixels(deltas, labels):
-
-    deltas = labels * deltas
-    counts, convergence = follow_deltas(deltas)
-
-    return counts, convergence
-
-
-def follow_deltas(deltas):
+def colocalize_pixels(deltas):
 
     i, j = np.arange(deltas.shape[0]), np.arange(deltas.shape[1])
     ii, jj = np.meshgrid(i, j, indexing='ij')
@@ -52,7 +31,7 @@ def follow_deltas(deltas):
     vmap_count_convergence = vmap(vmap(_count_convergence, in_axes=(None, None, 0)), in_axes=(None, 0, None))
     counts = vmap_count_convergence(convergence, i, j)
 
-    return counts, convergence
+    return counts
 
 
 def _count_convergence(convergence, i, j):
